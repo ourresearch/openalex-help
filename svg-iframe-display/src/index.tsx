fcf1bdf28804672f707e254f4d84bc38b91a02da
h1-the-swan <jason.portenoy@gmail.com>
2023-06-23 15:39:27 -0400

    trying to get diagram viewer to work

---
+++ svg-iframe-display/src/index.tsx
@@ -0,0 +1,66 @@
+import {
+    createIntegration,
+    createComponent,
+    FetchEventCallback,
+    RuntimeContext,
+  } from "@gitbook/runtime";
+  
+  type IntegrationContext = {} & RuntimeContext;
+  type IntegrationBlockProps = {};
+  type IntegrationBlockState = { url: string };
+  type IntegrationAction = { action: "@link.unfurl", url: string };
+  
+  const handleFetchEvent: FetchEventCallback<IntegrationContext> = async (
+    request,
+    context
+  ) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { api } = context;
+    const user = api.user.getAuthenticatedUser();
+  
+    return new Response(JSON.stringify(user));
+  };
+  
+  const exampleBlock = createComponent<
+     IntegrationBlockProps,
+     IntegrationBlockState,
+     IntegrationAction,
+     IntegrationContext
+  >({
+    componentId: "svg-iframe-display",
+    initialState: (props) => {
+      return {
+        url: ""
+      };
+    },
+    action: async (element, action, context) => {
+      switch (action.action) {
+        case "@link.unfurl":
+          const { url } = action;
+          console.log("@link.unfurl -- url: " + url);
+          element.state.url = url;
+          return {};
+      }
+    },
+    render: async (element, context) => {
+      return (
+        <block>
+            <webframe
+              source={{ url: `https://www.draw.io?lightbox=1&edit=_blank#Uhttps://raw.githubusercontent.com/ourresearch/openalex-help/main/.gitbook/assets/locations_screenshot_annotate.drawio` }}
+              aspectRatio={655 / 1030}
+            />
+          <image source={{
+            url: element.state.url
+          }} 
+          aspectRatio={655 / 1030}
+          />
+        </block>
+      );
+    },
+  });
+  
+  export default createIntegration({
+    fetch: handleFetchEvent,
+    components: [exampleBlock],
+    events: {},
+  });
